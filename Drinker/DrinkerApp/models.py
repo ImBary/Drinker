e1e8c1c7dca60ca006175296072ef47373a5df4e
ImBary <bartosz.sm@outlook.com>
2025-03-23 10:43:45 +0100

    migracja bazy

--- Drinker/DrinkerApp/models.py
+++ Drinker/DrinkerApp/models.py
@@ -5,6 +5,34 @@ from django.contrib.auth.models import User
 
 #https://www.youtube.com/watch?v=UxTwFMZ4r5k
 class Drink(models.Model):
+
+    TASTE_CHOICES = [
+        ('sweet', 'Sweet'),
+        ('bitter', 'Bitter'),
+        ('sour', 'Sour'),
+        ('salty', 'Salty'),
+        ('umami', 'Umami'),
+    ]
+    
+    STRENGTH_CHOICES = [
+        ('non-alcoholic', 'Non-Alcoholic'),
+        ('light', 'Light'),
+        ('medium', 'Medium'),
+        ('strong', 'Strong'),
+    ]
+    
+    TEMPERATURE_CHOICES = [
+        ('hot', 'Hot'),
+        ('cold', 'Cold'),
+        ('warm', 'Warm'),
+    ]
+    
+    COMPLEXITY_CHOICES = [
+        ('simple', 'Simple'),
+        ('moderate', 'Moderate'),
+        ('complex', 'Complex'),
+    ]
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     photo = models.ImageField(upload_to='images/')
@@ -12,6 +40,12 @@ class Drink(models.Model):
     instructon = models.TextField()
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+
+    taste_type = models.CharField(max_length=10, choices=TASTE_CHOICES, default='sweet')
+    strength = models.CharField(max_length=15, choices=STRENGTH_CHOICES, default='non-alcoholic')
+    temperature = models.CharField(max_length=10, choices=TEMPERATURE_CHOICES, default='cold')
+    complexity = models.CharField(max_length=10, choices=COMPLEXITY_CHOICES, default='simple')
+
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
